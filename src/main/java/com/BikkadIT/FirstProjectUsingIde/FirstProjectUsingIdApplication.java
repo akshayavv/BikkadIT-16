d0df85ad4ef978a06840a729a7ca1b8943e36602
Akshayavv <akshayakv00@gmail.com>
2024-09-25 13:20:18 +0400

    v1:8-132464, added maven security project

---
+++ src/main/java/com/BikkadIT/FirstProjectUsingIde/FirstProjectUsingIdApplication.java
@@ -0,0 +1,15 @@
+package com.BikkadIT.FirstProjectUsingIde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectUsingIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectUsingIdApplication.class, args);
+		
+		System.out.println("Working Fine");
+	}
+
+}
